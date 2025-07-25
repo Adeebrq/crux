105fb89309bbbdfb9852ac672464bb828bd44418
Adeeb Quraishi <adeebrq@gmail.com>
2025-07-25 16:55:39 +0530

    bug(deployment): unused variables

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -145,7 +145,7 @@ export function NavbarDemo() {
 
           <MobileNavMenu
             isOpen={isMobileMenuOpen}
-            onClose={() => setIsMobileMenuOpen(false)}
+            // onClose={() => setIsMobileMenuOpen(false)}
           >
             {navItems.map((item, idx) => (
               <a
